2baf5b21ded0799a623938146958c614e62be00b
davidpasek <david.pasek@gmail.com>
2022-09-28 16:14:11 +0200

    Polishing configuration

--- tf/vds-01.tf
+++ tf/vds-01.tf
@@ -1,3 +1,32 @@
+variable "vsphere_server" {
+  description = "Hostname of vCenter"
+  type        = string
+  default     = "vc01.home.uw.cz"
+}
+
+variable "vsphere_user" {
+  description = "User name of vSphere administrator"
+  type        = string
+  default     = "administrator@uw.cz"
+}
+
+variable "vsphere_password" {
+  description = "Password of vSphere administrator"
+  type        = string
+  default     = "VMware1!"
+}
+
+provider "vsphere" {
+  user                 = var.vsphere_user
+  password             = var.vsphere_password
+  vsphere_server       = var.vsphere_server
+  allow_unverified_ssl = true
+}
+
+data "vsphere_datacenter" "datacenter" {
+  name = "DC-HOME"
+}
+
 variable "esxi_hosts" {
   default = [
     "esx21.home.uw.cz",
@@ -7,49 +36,36 @@ variable "esxi_hosts" {
   ]
 }
 
-variable "network_interfaces" {
-  default = [
-    "vmnic0",
-    "vmnic1",
-    "vmnic2",
-    "vmnic3",
-  ]
-}
-
-data "vsphere_datacenter" "datacenter" {
-  name = "DC-HOME"
-}
-
 data "vsphere_host" "host" {
   count         = length(var.esxi_hosts)
   name          = var.esxi_hosts[count.index]
   datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
-resource "vsphere_distributed_virtual_switch" "vds" {
+resource "vsphere_distributed_virtual_switch" "vds-01" {
   name          = "vds-01"
   datacenter_id = data.vsphere_datacenter.datacenter.id
 
   uplinks         = ["uplink1", "uplink2"]
   active_uplinks  = ["uplink1", "uplink2"]
 
-  host {
-    host_system_id = data.vsphere_host.host.0.id
-    devices        = ["${var.network_interfaces}"]
-  }
+#  host {
+#    host_system_id = data.vsphere_host.host.0.id
+#    devices        = ["vmnic0","vmnic1"]
+#  }
 
-  host {
-    host_system_id = data.vsphere_host.host.1.id
-    devices        = ["${var.network_interfaces}"]
-  }
+#  host {
+#    host_system_id = data.vsphere_host.host.1.id
+#    devices        = ["vmnic0","vmnic1"]
+#  }
 
   host {
     host_system_id = data.vsphere_host.host.2.id
-    devices        = ["${var.network_interfaces}"]
+    devices        = ["vmnic0","vmnic1"]
   }
 
   host {
-    host_system_id = data.vsphere_host.host.4.id
-    devices        = ["${var.network_interfaces}"]
+    host_system_id = data.vsphere_host.host.3.id
+    devices        = ["vmnic0","vmnic1"]
   }
 }
